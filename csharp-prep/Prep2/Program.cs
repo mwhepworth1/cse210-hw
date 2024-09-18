2b0ab5fbe40f851257283a67c5c1a30199555890
mwhepworth1 <hep21001@byui.edu>
2024-09-18 13:42:17 -0600

    Completed the Prep1 and Prep2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+        string letterGrade;
+
+        if (grade >= 93)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 90)
+        {
+            letterGrade = "A-";
+        }
+        else if (grade >= 87)
+        {
+            letterGrade = "B+";
+        }
+        else if (grade >= 83)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B-";
+        }
+        else if (grade >= 77)
+        {
+            letterGrade = "C+";
+        }
+        else if (grade >= 73)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C-";
+        }
+        else if (grade >= 67)
+        {
+            letterGrade = "D+";
+        }
+        else if (grade >= 63)
+        {
+            letterGrade = "D";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D-";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You got a(n) {letterGrade}!");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Also, congratulations on passing!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, it looks like you didn't pass this time.");
+        }
     }
 }
---
+++ sandbox/The Boring Sandbox/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace The_Boring_Sandbox;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
