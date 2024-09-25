d4c97254995f624d3755eeaba3043fffea1efe51
mwhepworth1 <hep21001@byui.edu>
2024-09-25 13:35:28 -0600

    Completed Prep5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = squaredNumber(number);
+        DisplayResult(name, squared);
+    }
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName() {
+        Console.WriteLine("What is your name?");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber() {
+        Console.WriteLine("Enter your favorite number:");
+        return int.Parse(Console.ReadLine());
+    }
+    static int squaredNumber(int number) {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squaredNumber) {
+        Console.WriteLine($"Hello {name}, your square number is {squaredNumber}");
     }
 }
